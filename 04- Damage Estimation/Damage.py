4f1fd07ec351e2349094b14194c4a52627345512
Yasser Bigdeli <yasse.bigdeli@gmail.com>
2021-12-01 09:41:46 -0800

    2nd commit

--- 04- Damage Estimation/Damage.py
+++ 04- Damage Estimation/Damage.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
 class Damage(object):
-    '''
-    This program calculate the percentage of damage to a structure 
-    based on its average and max floor accelerations and drifts 
-    '''
+    """
+    This program calculate the percentage of damage to a structure
+    based on its average and max floor accelerations and drifts
+    """
 
     def __init__(self, x=[1, 2, 3], y=[-1, -3, -5], Degree_pol=3):
         self.x = x
@@ -34,7 +33,7 @@ fixed = [1.27, 2.6075, 1.67590]
 LRB = [0.47, 0.4288, 0.28]
 TFP = [0.16, 0.16, 0.13]
 
-L = fixed  # select type out of above  
+L = fixed  # select type out of above
 # ___________________________________________________________
 
 floor_acc = L[0]  # 0-3 sec median floor acceleration
@@ -46,7 +45,7 @@ Floor_drift = L[2]  # Average drift
 Acc = [0, 0.30, 0.40, 0.60, 1, 1.3, 1.501, 1.502, 1.503, 1.504, 1.505]  # floor Acc
 AccD = [0, 1.6, 2.7, 4.2, 10, 14, 16, 16, 16, 16, 16]  # Damage
 
-#Calculate damage 
+# Calculate damage
 DMG1 = Damage(Acc, AccD, Degree_pol=3)
 DMG1.PltCrv()
 [Acc_f, damage_f_a] = DMG1.DMG_calc(floor_acc)
@@ -61,7 +60,7 @@ print(
 
 
 #  Average drift in % __________________________________
-Drift = [ 0, 0.025, 0.050, 0.070, 0.10, 0.13, 0.20, 0.30, 0.5, 0.67, 1, 1.5, 2, 4]  
+Drift = [0, 0.025, 0.050, 0.070, 0.10, 0.13, 0.20, 0.30, 0.5, 0.67, 1, 1.5, 2, 4]
 
 # floor drift
 DriftD = [0, 0, 0, 0, 0, 0, 0.50, 1, 1.2, 3.4, 5.9, 11.5, 13, 17]  # Damage in %
@@ -78,7 +77,7 @@ print(
 
 #  Maximum drift in %
 Drift = [0, 0.13, 0.20, 0.30, 1, 2, 3, 5]  # floor drift
-DriftD = [0, 0, 0.50, 1, 2, 10, 13.5, 17]  # Damge in %
+DriftD = [0, 0, 0.50, 1, 2, 10, 13.5, 17]  # Damage in %
 
 DMG2 = Damage(Drift, DriftD, Degree_pol=3)
 DMG2.PltCrv()
